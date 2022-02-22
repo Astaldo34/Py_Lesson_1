f3cf6727684cd49546cd709c004e0aea2ad71a44
Astaldo <astaldo07@mail.ru>
2022-02-22 22:47:16 +0300

    done task_1_3

--- Shmelkov_Dmitry_dz_1/task_1_3.py
+++ Shmelkov_Dmitry_dz_1/task_1_3.py
@@ -1,2 +1,16 @@
-for n in range(100):
-    print(n)
+for n in range(101):
+    if n == 0:
+        continue
+    elif n == 1:
+        print(f'{n} процент')
+    elif n > 4 and n <= 20:
+        print(f'{n} процентов')
+    elif n > 20:
+        if str(n)[1] == "1" :
+            print(f'{n} процент')
+        elif str(n)[1] == "2" or str(n)[1] == "3" or str(n)[1] == "4":
+            print(f'{n} процента')
+        else :
+            print(f'{n} процентов')
+    else:
+        print(f'{n} процента')
