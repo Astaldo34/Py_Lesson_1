663cccd5ea8efcd03cb454a53a51b1ec044b283a
Astaldo <astaldo07@mail.ru>
2022-02-22 22:22:40 +0300

    done task_1_2

--- Shmelkov_Dmitry_dz_1/task_1_2.py
+++ Shmelkov_Dmitry_dz_1/task_1_2.py
@@ -1,1 +1,25 @@
+list_num = []
+# task a
+for num in range(100):
+    if num % 2 > 0:
+        num_cube = num ** 3
+        sum_numbers = 0
+        for i in str(num_cube):
+            sum_numbers = sum_numbers + int(i)
+        if sum_numbers % 7 == 0:
+            list_num.append(num_cube)
 
+print(list_num)
+
+list_num = []
+# task b
+for num in range(100):
+    if num % 2 > 0:
+        num_cube = num ** 3 + 17
+        sum_numbers = 0
+        for i in str(num_cube):
+            sum_numbers = sum_numbers + int(i)
+        if sum_numbers % 7 == 0:
+            list_num.append(num_cube)
+
+print(list_num)
