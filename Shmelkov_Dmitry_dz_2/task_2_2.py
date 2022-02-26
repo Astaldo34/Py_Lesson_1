bfcb9e03216285937baa42a106b04f7eed3abba6
Astaldo <astaldo07@mail.ru>
2022-02-26 15:51:02 +0300

    done task_2_2

--- Shmelkov_Dmitry_dz_2/task_2_2.py
+++ Shmelkov_Dmitry_dz_2/task_2_2.py
@@ -1,4 +1,26 @@
 list_name = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+new_list = []
+for index, item in enumerate(list_name):
+    for i in item:
+        if i == "+":
+            list_name[index] = '"+05"'
+    if item.isnumeric():
+        print(item)
+        if (len(item) < 2):
+            list_name[index] = '0' + list_name[index]
+        list_name[index] = '"' + list_name[index] + '"'
 
-for item in list_name:
-    break
+    # if item.isnumeric():
+    #     print(isinstance(item, str))
+    #     print(item)
+    #     if (len(item) < 2):
+    #         list_name[index] = '0' + list_name[index]
+    #     list_name[index] = '"' + list_name[index] + '"'
+
+print(list_name)
+str_list = ' '.join(list_name)
+print(str_list)
+
+"""
+Не понял как выявить число "+5"
+"""
